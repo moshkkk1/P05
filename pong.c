cff7cb04ec374ed7e28fa75b7428308b2599e364
Estrella Monster <estrellm@is-a2.stw.21-school.ru>
2025-12-19 18:26:03 +0300

    clang-edit

--- pong.c
+++ pong.c
@@ -20,9 +20,8 @@ int score1 = 0;  // счёт игрока 1 (слева)
 int score2 = 0;  // счёт игрока 2 (справа)
 
 /*
-Инициализация ракеток, мяча и очков. В ракетках мы инициализируем верхнюю черту, а середину и дно будем рисовать
-уже в самом поле.
-Цифры не рандомные, а научно натыканные, чтоб ракетки стояли в середине.
+Инициализация ракеток, мяча и очков. В ракетках мы инициализируем верхнюю черту, а середину и дно будем
+рисовать уже в самом поле. Цифры не рандомные, а научно натыканные, чтоб ракетки стояли в середине.
 */
 void init(void) {
     y_pad1 = (HEIGHT - 3) / 2;
@@ -44,34 +43,34 @@ void init(void) {
 */
 void move_pad(char z) {
     switch (z) {
-    case 'A':
-    case 'a':
-        if (y_pad1 > 1) {
-            y_pad1--;
-        }
-        break;
-    case 'Z':
-    case 'z':
-        if (y_pad1 < 21) {
-            y_pad1++;
-        }
-        break;
-    case 'K':
-    case 'k':
-        if (y_pad2 > 1) {
-            y_pad2--;
-        }
-        break;
-    case 'M':
-    case 'm':
-        if (y_pad2 < 21) {
-            y_pad2++;
-        }
-        break;
-    case ' ':
-        break;
-    default:
-        break;
+        case 'A':
+        case 'a':
+            if (y_pad1 > 1) {
+                y_pad1--;
+            }
+            break;
+        case 'Z':
+        case 'z':
+            if (y_pad1 < 21) {
+                y_pad1++;
+            }
+            break;
+        case 'K':
+        case 'k':
+            if (y_pad2 > 1) {
+                y_pad2--;
+            }
+            break;
+        case 'M':
+        case 'm':
+            if (y_pad2 < 21) {
+                y_pad2++;
+            }
+            break;
+        case ' ':
+            break;
+        default:
+            break;
     }
 }
 
@@ -93,9 +92,10 @@ void move_ball(void) {
     }
 
     /*
-    Отскок от ракеток. Тут подробнее. Ракетка имеет три секции. Если мяч прилетел в верхнюю часть ракетки, то и отскок будет вверх, если
-    в нижнюю часть, то вниз. Это добавлено специально, ибо без данного функционала, игра превращалась в бесконечный алгоритм повторяющихся действий.
-    А так игра получает хоть небольшую дозу неопределенности.
+    Отскок от ракеток. Тут подробнее. Ракетка имеет три секции. Если мяч прилетел в верхнюю часть ракетки, то
+    и отскок будет вверх, если в нижнюю часть, то вниз. Это добавлено специально, ибо без данного функционала,
+    игра превращалась в бесконечный алгоритм повторяющихся действий. А так игра получает хоть небольшую дозу
+    неопределенности.
     */
     // Левая ракетка
     if (x_ball == 1 && y_ball >= y_pad1 && y_ball < y_pad1 + 3) {
@@ -103,8 +103,7 @@ void move_ball(void) {
         int hit_pos = y_ball - y_pad1;
         if (hit_pos == 0) {
             dy = -1;
-        }
-        else if (hit_pos == 2) {
+        } else if (hit_pos == 2) {
             dy = 1;
         }
     }
@@ -114,8 +113,7 @@ void move_ball(void) {
         int hit_pos = y_ball - y_pad2;
         if (hit_pos == 0) {
             dy = -1;
-        }
-        else if (hit_pos == 2) {
+        } else if (hit_pos == 2) {
             dy = 1;
         }
     }
@@ -130,8 +128,7 @@ void move_ball(void) {
         y_ball = HEIGHT / 2;
         dx = 1;
         dy = 1;
-    }
-    else if (x_ball >= WIDTH) {
+    } else if (x_ball >= WIDTH) {
         // Очки игроку 1
         score1++;
         x_ball = WIDTH / 2;
@@ -148,46 +145,43 @@ void move_ball(void) {
 char read_command(void) {
     char c;
     int valid_input = 0;
-    
+
     while (!valid_input) {
         printf("\nВведите команду (A/Z - игрок 1, K/M - игрок 2, Space - пропуск ход): ");
         c = getchar();
-        
+
         // Пропускаем символы переноса строки
         while (getchar() != '\n');
-        
+
         // Проверяем корректность ввода
-        if (c == 'A' || c == 'a' || c == 'Z' || c == 'z' || 
-            c == 'K' || c == 'k' || c == 'M' || c == 'm' || 
+        if (c == 'A' || c == 'a' || c == 'Z' || c == 'z' || c == 'K' || c == 'k' || c == 'M' || c == 'm' ||
             c == ' ') {
             valid_input = 1;
         } else {
             printf("Неверная команда! Используйте A, Z, K, M или Space.\n");
         }
     }
-    
+
     return c;
 }
 
 void playground(void) {
     printf("=== Player 1: %d  |  Player 2: %d === (first to %d wins)\n", score1, score2, WIN);
-    
+
     // Прямая отрисовка без массива
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
             char cell = ' ';
-            
+
             // Границы поля
             if (i == 0 || i == HEIGHT - 1) {
                 if (j == 0 || j == WIDTH - 1)
                     cell = '+';
                 else
                     cell = '-';
-            }
-            else if (j == 0 || j == WIDTH - 1) {
+            } else if (j == 0 || j == WIDTH - 1) {
                 cell = '|';
-            }
-            else {
+            } else {
                 // Левая ракетка (x = 1)
                 if (j == 1 && i >= y_pad1 && i < y_pad1 + 3) {
                     cell = '|';
@@ -201,7 +195,7 @@ void playground(void) {
                     cell = 'O';
                 }
             }
-            
+
             printf("%c", cell);
         }
         printf("\n");
@@ -228,7 +222,7 @@ int main(void) {
     printf("Первый игрок, набравший %d очков, побеждает!\n", WIN);
     printf("Нажмите Enter для начала игры...\n");
     getchar();  // Ждем нажатия Enter
-    
+
     init();
     char cmd;
     while (score1 < WIN && score2 < WIN) {
@@ -237,16 +231,16 @@ int main(void) {
         move_pad(cmd);
         move_ball();
     }
-    
+
     printf("\nИгра завершена!\n");
     printf("Player 1: %d очков\n", score1);
     printf("Player 2: %d очков\n", score2);
-    
+
     if (score1 == WIN) {
         printf("Победитель: Player 1!\n");
     } else if (score2 == WIN) {
         printf("Победитель: Player 2!\n");
     }
-    
+
     return 0;
 }
